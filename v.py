50f76822edb980e27a4ccf26c089c94413425b95
DANFASO COUTURE <admin@verdureclimate.com>
2024-12-01 13:30:13 +0100

    Updated code for better error handling

--- v.py
+++ v.py
@@ -61,7 +61,7 @@ def create_app():
 
     # Sidebar Configuration
     with st.sidebar:
-        st.image("https://github.com/VerdureClimateLtd/verdure_temp_score_tool/blob/main/verd.png", width=300)  # Replace "logo.png" with the path to your logo
+        st.image("https://github.com/VerdureClimateLtd/verdure_temp_score_tool/blob/main/verd.png")
         st.markdown("<h2 style='display:inline; font-family:Arial;'>Verdure Climate</h2>", unsafe_allow_html=True)
 
         #st.markdown("---")
